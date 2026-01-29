be67816b1cf9d9ec804cf372496a46e94ccbc950
David-Tarlos <david.tarlos@edu.tbz.ch>
2026-01-29 10:30:20 +0100

    add new title

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -71,7 +71,7 @@ export default function HomePage() {
                             letterSpacing: '-0.02em',
                         }}
                     >
-                        OurOwnSpace
+                        OurSpace
                     </Typography>
 
                     {/* Untertitel */}
